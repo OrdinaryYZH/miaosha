8501ca9bb9d9bde1c0f228045e2400bc0c3395c0
genericyzh <yaozihuan@163.com>
2018-12-28 15:05:09 +0800

    【Update】

--- src/main/java/com/genericyzh/miaosha/MiaoshaApplication.java
+++ src/main/java/com/genericyzh/miaosha/MiaoshaApplication.java
@@ -1,9 +1,7 @@
 package com.genericyzh.miaosha;
 
-import com.genericyzh.miaosha.redis.RedisSpringConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 //@Import({RedisSpringConfiguration.class})
