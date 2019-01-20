abf529a8c833202d58f674598726487a7637378f
genericyzh <yaozihuan@163.com>
2019-01-20 23:40:02 +0800

    【Update】去掉PublicException msg属性

--- src/main/java/com/genericyzh/miaosha/common/exception/PublicException.java
+++ src/main/java/com/genericyzh/miaosha/common/exception/PublicException.java
@@ -8,36 +8,30 @@ import com.genericyzh.miaosha.common.result.ResultCode;
  */
 public class PublicException extends RuntimeException {
     private int code = 999;
-    private String msg;
     private String developerMessage;
 
     public PublicException(String message) {
         super(message);
-        this.msg = message;
     }
 
     public PublicException(ResultCode code) {
         super(code.message());
         this.code = code.code();
-        this.msg = code.message();
     }
 
     public PublicException(ResultCode code, String message) {
         super(message);
         this.code = code.code();
-        this.msg = message;
     }
 
     public PublicException(int code, String message) {
         super(message);
         this.code = code;
-        this.msg = message;
     }
 
     public PublicException(int code, String message, String developerMessage) {
         super(message);
         this.code = code;
-        this.msg = message;
         this.developerMessage = developerMessage;
     }
 
@@ -49,13 +43,6 @@ public class PublicException extends RuntimeException {
         this.code = code;
     }
 
-    public String getMsg() {
-        return msg;
-    }
-
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
 
     public String getDeveloperMessage() {
         return developerMessage;
